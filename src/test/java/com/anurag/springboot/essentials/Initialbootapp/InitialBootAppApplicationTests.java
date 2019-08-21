9b5de215c717129a36a1318ee06c6ca9763d719c
Eswar Musiboyina <eswar@Eswars-MBP.attlocal.net>
2019-08-21 01:00:25 -0500

    Initial commit with One endpoing and some profile changes

---
+++ src/test/java/com/anurag/springboot/essentials/Initialbootapp/InitialBootAppApplicationTests.java
@@ -0,0 +1,16 @@
+package com.anurag.springboot.essentials.Initialbootapp;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class InitialBootAppApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
